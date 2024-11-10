5216c2bc49b2084db22c6f34754fedba4b904382
Alan <alanpramil7@gmail.com>
2024-11-10 19:25:14 +0530

    added nvim statusline

---
+++ nvim/.config/nvim/lua/alan/lazy/mini-statusline.lua
@@ -0,0 +1,14 @@
+return {
+  'echasnovski/mini.statusline',
+  dependencies = {
+    'echasnovski/mini.icons',
+  },
+  config = function ()
+    require('mini.statusline').setup {
+      content = {
+        active = require('noirbuddy.plugins.mini-statusline').active,
+        inactive = require('noirbuddy.plugins.mini-statusline').inactive,
+      },
+    }
+  end,
+}
